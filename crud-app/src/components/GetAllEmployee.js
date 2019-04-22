085af273636e2c5e07c9ea47dd2e3e9df96824d1
ElizaIra <you@example.com>
2019-04-22 13:06:22 +0800

    Add input employee details shown in console log

--- crud-app/src/components/GetAllEmployee.js
+++ crud-app/src/components/GetAllEmployee.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios'; 
-// import DeleteEmployee from './components/DeleteEmployee';
-
+// import DeleteEmployee from './DeleteEmployee';
+import CreateEmployee from './CreateEmployee';
 
 import '../App.css';
 
@@ -10,29 +10,48 @@ class AllEmployee extends Component {
   constructor(props) {
     super(props);
     this.state = { employees: [] };
+
+    this.onAdd = this.onAdd.bind(this);
   }
-  
+
   componentDidMount() {
     try {
       axios({
         method: 'get',
-        url: 'http://dummy.restapiexample.com/api/v1/employees'
+        url: 'http://dummy.restapiexample.com/api/v1/employees' 
       }).then(response => {
         console.log(response.data);
         this.setState({employees: response.data});
       });
+      
     } catch (err) {
       console.log('Fetching all employee details');
       console.log(err);
     }
+
+  
+    
+  }
+  onAdd(employee_name, employee_age, employee_salary){
+    console.log(employee_name, employee_age, employee_salary);
+    // const products = this.GetAllEmployee();
+    // products.push({
+    //   employee_name,
+    //   employee_age,
+    //   employee_salary
+    // });
+    // this.setState({employees});
   }
   
   render() {
       return (
+        
       <div className="container">
         <div className="modal-content">
           <div className="modal-header">
-            Employee Details
+             Employee Details 
+            <CreateEmployee onAdd={this.onAdd} />
+           
           </div>
           <div className="modal-body">
           <table id="table-content">
@@ -48,14 +67,18 @@ class AllEmployee extends Component {
               this.state.employees.map(employees => (
                 
               <tr key={employees.id}>
-              
+               
                 <td>{employees.employee_name}</td>
                 <td>{employees.employee_age}</td>
                 <td>{employees.employee_salary}</td>
                 <td>
+                
+                
                   <button className="btn-view"><i className="fa fa-user"></i></button>
-                  <button className="btn-add"><i className="fa fa-plus"></i></button>
-                  <button className="btn-delete"><i className="fa fa-trash"></i></button>
+                  
+               
+                  <button className="btn-delete"><i className="fa fa-trash"></i>
+                  </button>
                 </td>
               </tr>
 
