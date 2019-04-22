5ede6adc9ca9270fc118eb20e49c2f4696208795
ElizaIra <you@example.com>
2019-04-22 13:11:22 +0800

    Main page

---
+++ crud-app/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import GetAllEmployee from './components/GetAllEmployee';
+
+// import UpdateEmployee from './components/UpdateEmployee';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+
+      <div className = 'App'>
+       <GetAllEmployee />
+    
+      </div>
+      );
+  }
+    
+
+  }
+
+
+  
+
+  
+export default App;
