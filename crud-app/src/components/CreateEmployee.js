ed6a2a53b0ee998da7fbb6d782f0eba67b41b3bd
ElizaIra <you@example.com>
2019-04-22 13:09:06 +0800

    Create Add Function

---
+++ crud-app/src/components/CreateEmployee.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import axios from 'axios'; 
+
+// import modal from 'modal';
+
+class CreateEmployee extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    
+
+    onSubmit(event){
+        event.preventDefault();
+
+        this.props.onAdd(this.nameInput.value, this.ageInput.value, this.salaryInput.value);
+
+        //clear fields in the input
+        this.nameInput.value='';
+        this.ageInput.value='';
+        this.salaryInput.value='';
+    }
+
+    CreateEmployee(data){
+        try {
+            axios({
+            method: 'post',
+            url: 'http://dummy.restapiexample.com/api/v1/create',
+            data: data
+            }).then(response => {
+            console.log('Adding Employee Data');
+            console.log(response.data);
+            });
+        } catch (err) {
+            console.log('Error on adding selected employee data');
+            console.log(err);
+        }
+    }
+    render() {  
+
+        return (
+        <form onSubmit={this.onSubmit}>
+             <div className="container">
+            <input placeholder="Name" className="input-box" ref={nameInput => this.nameInput = nameInput} />
+            <input placeholder="Age" className="input-box"ref={ageInput => this.ageInput = ageInput} />
+            <input placeholder="Salary" className="input-box" ref={salaryInput => this.salaryInput = salaryInput} />
+           
+           
+            <button className="btn-add" id="btn-add">
+                 Add
+            </button>
+            
+        </div>
+        </form>
+       
+        );      
+    }
+}
+
+export default CreateEmployee;
